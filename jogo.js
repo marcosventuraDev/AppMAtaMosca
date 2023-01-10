12b6ea8f68021683ec327e3e217bde84f1977920
marcos ventura <marcosventura.dev@gmail.com>
2023-01-10 12:27:42 -0300

    criando cenario

--- jogo.js
+++ jogo.js
@@ -12,32 +12,43 @@ function ajustaTAmanhoPalcoJogo() {
 ajustaTAmanhoPalcoJogo()
 
 function posicaoRandomica() {
-    let positionX = Math.floor(Math.random() * largura) - 90
-    let positionY = Math.floor(Math.random() * altura) - 90
 
-    positons = [positionX, positionY]
+    //removar mosquito anterior caso exista
+    if(document.getElementById('mosca')){
+        document.getElementById('mosca').remove();
+    }
 
-   positionX = positionX < 0 ? 0 : positionX
-    positionY = positionY < 0 ? 0 : positionY 
+
+    let positionX = Math.floor(Math.random() * largura) - 90;
+    let positionY = Math.floor(Math.random() * altura) - 90;
+
+    positons = [positionX, positionY];
+
+   positionX = positionX < 0 ? 0 : positionX;
+    positionY = positionY < 0 ? 0 : positionY ;
 
 
 
     //criando a mosta de forma programática
 
-    let mosca = document.createElement('img')
-    mosca.src = './imagens/mosca.png'
-    mosca.className = tamanhoAleatorio() + " " + ladoAleatorio()
-    mosca.style.position = 'absolute'
-    mosca.style.left = positionX + 'px'
-    mosca.style.top = positionY + 'px'
+    let mosca = document.createElement('img');
+    mosca.src = './imagens/mosca.png';
+    mosca.className = tamanhoAleatorio() + " " + ladoAleatorio();
+    mosca.style.position = 'absolute';
+    mosca.style.left = positionX + 'px';
+    mosca.style.top = positionY + 'px';
+    mosca.id = 'mosca';
+    mosca.onclick= function(){
+        this.remove()
+    }
 
-    document.body.appendChild(mosca)
-    tamanhoAleatorio()
+    document.body.appendChild(mosca);
+    tamanhoAleatorio();
 }
 
 function tamanhoAleatorio(){
-    let classe = Math.floor(Math.random()*3)
-    console.log(classe)
+    let classe = Math.floor(Math.random()*3);
+    console.log(classe);
 
     switch(classe){
         case 0:
@@ -50,7 +61,7 @@ function tamanhoAleatorio(){
 }
 
 function ladoAleatorio(){
-    let classe = Math.floor(Math.random()*2)
+    let classe = Math.floor(Math.random()*2);
     switch(classe){
         case 0:
             return 'ladoA';
